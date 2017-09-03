6d9570bc1a7f1f8520ae3f800c6dd11dfa90eed3
vasujava75 <vasujava75@gmail.com>
2017-09-03 14:52:20 +0530

    loader class changes added

--- PersonalityWebApp/src/main/java/com/affinitas/personApp/serviceImpl/JsonDataLoaderServiceImpl.java
+++ PersonalityWebApp/src/main/java/com/affinitas/personApp/serviceImpl/JsonDataLoaderServiceImpl.java
@@ -9,6 +9,7 @@ import javax.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
@@ -23,46 +24,59 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class JsonDataLoaderServiceImpl implements JsonDataLoaderService {
-	
+
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	ResourceLoader resourceLoader;
+	
 	@Autowired
 	QuestionsRepository questionsRepository;
+	
 	@Autowired
 	CategoryRepository categoryRepository;
+	
+	@Value("${fileName}")
+	private String fileName;
 
 	@Transactional
 	public JsonDataDto loadDataFromFileToH2Db() {
 		ObjectMapper mapper = new ObjectMapper();
 		JsonDataDto jsonDataObject = null;
 		try {
-			jsonDataObject = mapper.readValue(resourceLoader.getResource("classpath:dataFile.json").getFile(),
+			jsonDataObject = mapper.readValue(resourceLoader.getResource("classpath:"+fileName).getFile(),
 					JsonDataDto.class);
 			if (jsonDataObject != null) {
 				List<QuestionDto> questionsDtos = jsonDataObject.getQuestions();
 				List<Questions> questionsList = new ArrayList<>();
 				List<Category> categorieList = new ArrayList<>();
-				questionsDtos.forEach(questionDto -> {
-					Questions question = new Questions();
-					question.setQuestion(questionDto.getQuestion());
-					question.setCategory(questionDto.getCategory());
-					question.setQuestionType(questionDto.getQuestion_type().getType());
-					question.setOptions(questionDto.getQuestion_type().getOptions().toString());
-					questionsList.add(question);
-				});
-				questionsRepository.save(questionsList);
-				jsonDataObject.getCategories().forEach( categoryItem-> {
-					Category category = new Category();
-					category.setCategory(categoryItem);
-					categorieList.add(category);
-				});
-				categoryRepository.save(categorieList);
+				createQuestionsDbFromJsonList(questionsDtos, questionsList);
+				createCategoriesDbFromJsonList(jsonDataObject, categorieList);
 			}
 		} catch (IOException e) {
 			logger.error("File read exception");
 		}
 		return jsonDataObject;
+	}
+
+	private void createQuestionsDbFromJsonList(List<QuestionDto> questionsDtos, List<Questions> questionsList) {
+		questionsDtos.forEach(questionDto -> {
+			Questions question = new Questions();
+			question.setQuestion(questionDto.getQuestion());
+			question.setCategory(questionDto.getCategory());
+			question.setQuestionType(questionDto.getQuestion_type().getType());
+			question.setOptions(questionDto.getQuestion_type().getOptions().toString());
+			questionsList.add(question);
+		});
+		questionsRepository.save(questionsList);
+	}
+
+	private void createCategoriesDbFromJsonList(JsonDataDto jsonDataObject, List<Category> categorieList) {
+		jsonDataObject.getCategories().forEach(categoryItem -> {
+			Category category = new Category();
+			category.setCategory(categoryItem);
+			categorieList.add(category);
+		});
+		categoryRepository.save(categorieList);
 	}
 }
