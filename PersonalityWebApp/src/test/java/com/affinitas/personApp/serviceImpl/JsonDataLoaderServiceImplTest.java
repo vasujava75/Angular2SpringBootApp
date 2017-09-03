b4a6ef484f8aa7a89913cf9969e1adfcb21fff09
vasujava75 <vasujava75@gmail.com>
2017-09-03 14:56:19 +0530

    commented code removed

--- PersonalityWebApp/src/test/java/com/affinitas/personApp/serviceImpl/JsonDataLoaderServiceImplTest.java
+++ PersonalityWebApp/src/test/java/com/affinitas/personApp/serviceImpl/JsonDataLoaderServiceImplTest.java
@@ -1,8 +1,7 @@
 package com.affinitas.personApp.serviceImpl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
